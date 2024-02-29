c9a6cee2faa58891d6a1ad98e6db86f86ba513ee
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-02-29 17:41:03 +0900

    Top.vueの作成

---
+++ src/components/Top.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button>ログアウト</button>
+    <button>マイページ</button>
+  </div>
+  <div>
+    <form>
+      <div class="form_element">
+        <label for="title">タイトル</label><br>
+        <input type="text" id="title">
+      </div>
+      <div class="form_element">
+        <label for="review">感想</label><br>
+        <textarea id="review"></textarea>
+      </div>
+      <input type="submit" value="送信">
+    </form>
+    <showList />
+  </div>
+</template>
+
+<script>
+import showList from './showList.vue';
+
+export default {
+  name: 'TopComp',
+  components: {
+    showList
+  },
+}
+</script>
+
+<style>
+form {
+  margin: 24px auto;
+}
+</style>

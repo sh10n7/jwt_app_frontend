7462caf080ec02350a431d082bea1ad58c7e13d8
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-02-29 17:40:29 +0900

    showList.vueの作成

---
+++ src/components/showList.vue
@@ -0,0 +1,34 @@
+<template>
+  <h2>レビュー一覧</h2>
+  <div>
+    <ul class="review-lists"> 
+      <li class="review-list">
+        <p class="review-title">タイトル</p>
+        <p>感想</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'showList',
+}
+</script>
+
+<style>
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+.review-list {
+  border:1px solid #333;
+  padding: 8px;
+}
+
+.review-title {
+  font-weight: 400;
+  margin: 8px 0;
+}
+</style>

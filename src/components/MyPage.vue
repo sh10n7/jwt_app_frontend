31838d87f2d16a97246caf0c7703159796886178
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-02-29 18:52:12 +0900

    コンポーネントの読み込み修正

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -1,9 +1,11 @@
-<h1>マイページ</h1>
-<h2>投稿一覧</h2>
-<ShowList />
+<template>
+  <h1>マイページ</h1>
+  <h2>投稿一覧</h2>
+  <ShowList />
 </template>
 
 <script>
+import ShowList from './ShowList.vue';
 export default {
   name: 'MyPage',
   components:{

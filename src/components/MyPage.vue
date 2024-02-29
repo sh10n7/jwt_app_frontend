4ca29720cf77886877307a17f8f9380c016f0d0f
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-03-01 01:29:59 +0900

    レビューのフィルタリング処理を追加

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -1,15 +1,33 @@
 <template>
   <h1>マイページ</h1>
-  <h2>投稿一覧</h2>
-  <ShowList />
+  <ShowList :books="filterBooks"/>
 </template>
 
 <script>
+import { onMounted, ref } from 'vue';
 import ShowList from './ShowList.vue';
+import apiClient from '../api/axios';
+
+
 export default {
   name: 'MyPage',
   components:{
     ShowList
+  },
+  setup(){
+    const filterBooks = ref([]);
+    
+    onMounted(async()=> {
+      try {
+        const response = await apiClient.get('/mybook', {
+          headers: {'Authorization': `Bearer ${localStorage.getItem('jwt')}`}
+        })
+        filterBooks.value = response.data;
+      }catch(error){
+        console.log('データの取得ができませんでした', error);
+      }
+    });
+    return { filterBooks }
   }
 }
 </script>

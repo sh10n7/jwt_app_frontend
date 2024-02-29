a301377c8f3afe5380843a1eb14b1b9c285874f6
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-02-29 18:11:22 +0900

    ログイン・新規登録画面の作成

---
+++ src/components/SIgnUp.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>新規登録</h1>
+  <input type="text" id="email" v-model="email" placeholder="email">
+  <input type="password" id="password" v-model="password" placeholder="password">
+  <button value="新規登録" @click="handleSignUp">新規登録</button>
+  <p>既にアカウントをお持ちの方はこちらへ</p>
+</template>
+
+<script>
+export default{
+  name: 'SignUp',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>ログイン</h1>
+  <input type="text" id="email" v-model="email" placeholder="email">
+  <input type="password" id="password" v-model="password" placeholder="password">
+  <button value="新規登録" @click="handleSignIn">ログイン</button>
+  <p>アカウント作成がお済みでない場合は、こちらへ</p>
+</template>
+
+<script>
+export default{
+  name: 'SignIn',
+}
+</script>
+
+<style>
+
+</style>
